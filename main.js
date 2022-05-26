0a83838b1bce171a908c750c9de166af50a8786f
mazarona <mazenfahem@gmail.com>
2022-05-26 03:52:27 +0200

    added bubble sort

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-var arrSize = 20;
+var arrSize = 50;
 var maxRange = 100;
 
 var arr = [];
@@ -8,12 +8,10 @@ var elements = [];
 var arrayAcess = 0;
 var comparsions = 0;
 var speed = 0;
-var called = false;
 
 var disblay = document.querySelector(".disblay");
 var numberOfComparisons = document.querySelector(".number-of-comparisons");
 var numberOfArrayAccess = document.querySelector(".number-of-array-access");
-var arrayContent = document.querySelector(".array-content p");
 var speedSlider = document.querySelector(".speed input");
 var disblayWidth = disblay.clientWidth;
 var disblayHeight = disblay.clientHeight;
@@ -22,13 +20,14 @@ speed = speedSlider.value;
 // 1) Populate the array with random numbers
 for (var i = 0; i < arrSize; i++) {
   arr.push(Math.floor(Math.random() * maxRange) + 1);
-  arrayContent.textContent += arr[i].toString() + " ";
 }
 // 2) create the css div elements
 createElements();
 /*** Event listeners ***/
 var selectionSortBtn = document.querySelector(".selection-sort");
 selectionSortBtn.addEventListener("click", selection_sort);
+var selectionSortBtn = document.querySelector(".bubble-sort");
+selectionSortBtn.addEventListener("click", bubble_sort);
 
 window.addEventListener("resize", () => {
   disblayWidth = disblay.clientWidth;
@@ -70,10 +69,6 @@ function updateState() {
 //selection_sort
 async function selection_sort() {
   speed = speedSlider.value;
-  if (called) {
-    return;
-  }
-  called = true;
   shuffleArray(arr);
   updateState();
   arrayAcess = 0;
@@ -105,6 +100,46 @@ async function selection_sort() {
   }
   for (var i = 0; i < arrSize; i++) {
     elements[i].style.backgroundColor = "white";
+  }
+  for (var i = 0; i < arrSize; i++) {
+    elements[i].style.backgroundColor = "green";
+    await waitforme(100 - speed);
+  }
+}
+
+async function bubble_sort() {
+  speed = speedSlider.value;
+  shuffleArray(arr);
+  updateState();
+  arrayAcess = 0;
+  comparsions = 0;
+  var temp;
+  for (var i = 0; i < arrSize - 1; i++) {
+    for (var k = 0; k < arrSize - 1 - i; k++) {
+      elements[k].style.backgroundColor = "red";
+      await waitforme(100 - speed);
+      if (arr[k] > arr[k + 1]) {
+        temp = arr[k + 1];
+        arr[k + 1] = arr[k];
+        arr[k] = temp;
+        elements[k].style.height =
+          ((arr[k] / maxRange) * disblayHeight).toString() + "px";
+        elements[k + 1].style.height =
+          ((arr[k + 1] / maxRange) * disblayHeight).toString() + "px";
+        arrayAcess += 4;
+        numberOfArrayAccess.textContent =
+          "Number Of Array Access: " + arrayAcess.toString();
+      }
+      comparsions++;
+      numberOfComparisons.textContent =
+        "Number Of Comparisons: " + comparsions.toString();
+      elements[k].style.backgroundColor = "white";
+    }
+    elements[arrSize - 1 - i].style.backgroundColor = "green";
+  }
+  for (var i = 0; i < arrSize; i++) {
+    elements[i].style.backgroundColor = "white";
+    //    elements[i].style.opacity = "50%";
   }
   for (var i = 0; i < arrSize; i++) {
     elements[i].style.backgroundColor = "green";
